2b0bf645afa93fdcb0526fd4d7fb1513aefab9dd
surajrockzz <penugondasuraj@gmail.com>
2025-01-19 14:51:07 -0600

    added themes and some classes for theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomePage from './components/HomePage';
+import { Separator } from '@/components/ui/separator';
+import { BrowserRouter, Routes, Route } from "react-router";
 
 
 function App() {
-  
 
   return (
-    <div className="App">
+    <div className="App ml-8 mr-8">
       <Header />
+      <Separator />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+          <Route path="/resume" element={<h1>Resume</h1>} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   )
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,19 @@
+function HomePage() {
+    return (
+        <>
+            <section className="intro h-52 text-foreground">
+                👋 Hi, I'm Suraj Penugonda
+            </section>
+            <section className="self-intro h-52 text-foreground">
+                LET ME INTRODUCE MYSELF
+                Full Stack Developer with expertise in React.js, Node.js, and AWS. Passionate about building scalable applications and exploring new technologies.
+            </section>
+            <section className="find-me h-52 text-foreground">
+                FIND ME ON
+                Feel free to connect with me
+            </section>
+        </>)
+}
+
+
+export default HomePage;
