048a257da4e83ef110de6be01e62d2047c177b13
surajrockzz <penugondasuraj@gmail.com>
2025-01-19 23:29:21 -0600

    modified some changes

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,10 @@
 import '@/css/HomePage.css';
 import ComputerMan from '@/assets/computer.png';
-import Title from '@/components/Title';
 import { useEffect, useState } from 'react';
 
 function HomePage() {
 
-    const differentTexts: string[] = ['Software Developer', ' ',  'Full Stack Developer', ' ', 'FrontEnd Developer', ' ', 'Backend Developer', ' '];
+    const differentTexts: string[] = ['Software Developer', 'Full Stack Developer', 'FrontEnd Developer', 'Backend Developer'];
     const [showText, setShowText] = useState(differentTexts[0])
     const [number, setNumber] = useState(0)
 
@@ -26,20 +25,28 @@ function HomePage() {
                     <div className='flex-1 content-center'>
                         <p className='text-xl text-center'>  Hi, There 👋</p>
                         <p className='text-xl text-center'>  I'm Suraj Penugonda </p>
-                        <p  className={'pl-56 typewriter text-2xl text-center inline ' + (showText.length > 0 ? 'playing': 'stopped') }> {showText} </p>
+                        <p className='pl-50 text-2xl text-center typewriter'> {showText} </p>
                     </div>
-                    <div className='flex-1'>
-                        <img src={ComputerMan} className="h-full w-full object-cover" alt="" />        
+                    <div className='flex-1 justify-items-center'>
+                        <img src={ComputerMan} className="h-full w-full object-cover computer-man" alt=" man image" />        
                     </div>
                 </div>
             </section>
             <section className="self-intro text-foreground">
-                LET ME INTRODUCE MYSELF
-                Full Stack Developer with expertise in React.js, Node.js, and AWS. Passionate about building scalable applications and exploring new technologies.
+                <div className='flex flex-col items-start p-[5%]'>
+                    <p className='text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]'>LET ME INTRODUCE MYSELF</p>
+                    <p className='max-w-2xl text-lg font-light text-foreground'>
+                        Full Stack Developer with expertise in React.js, Node.js, and AWS. Passionate about building scalable applications and exploring new technologies.
+                    </p>
+                </div>
             </section>
-            <section className="find-me text-foreground">
-                FIND ME ON
-                Feel free to connect with me
+            <section className="self-intro text-foreground">
+                <div className='flex flex-col gap-1 py-8 md:py-10 lg:py-12 p-[5%] items-center'>
+                    <p className='text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]'>FIND ME ON</p>
+                    <p className='max-w-2xl text-lg font-light text-foreground'>
+                    Feel free to connect with me
+                    </p>
+                </div>
             </section>
         </>)
 }
